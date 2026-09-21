73b96a041186b61276692034ebbb59561aec5e5b
priyagupta108 <priyagupta108@github.com>
2026-09-21 15:21:46 +0530

    Add TypeScript support to ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,15 @@
+import tsParser from "@typescript-eslint/parser";
+
 export default [
-    {
-      files: ["**/*.js"],
-      rules: {
-        semi: "error",
-        quotes: ["error", "double"],
-      },
+  {
+    files: ["**/*.js"],
+    rules: {
+      semi: "error",
+      quotes: ["error", "double"],
     },
-  ];
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: { parser: tsParser },
+  },
+];
