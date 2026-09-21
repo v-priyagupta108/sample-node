f08cfdd576dbb5417747ddca1f128f92659d3182
priyagupta108 <priyagupta108@github.com>
2026-09-21 15:26:40 +0530

    Refactor add function and clean up handler implementation

--- functions/a.ts
+++ functions/a.ts
@@ -1,13 +1,7 @@
-const a = (a: number, b: number): number => a + b;
+};
+
+const add = (a: number, b: number): number => a + b;
 
 export const handler = () => {
-  make javascript fail again
-  async function foo(things) {
-    const results = [];
-    for (const thing of things) {
-      // Bad: each loop iteration is delayed until the entire asynchronous operation completes
-      results.push(await bar(thing));
-    }
-    return baz(results);
-  }
+  return add(1, 2);
 };
